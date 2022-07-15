4da23c24b88f93d1d6cdebab51bd822b4025a38f
Haowen Wang <wang.h.w@outlook.com>
2022-07-15 23:35:17 +0800

    complete 200

---
+++ disjoint/200. 岛屿数量/NumIslands.java
@@ -0,0 +1,27 @@
+class NumIslands {
+	int m, n;
+	public int numIslands(char[][] grid) {
+		m = grid.length;
+		n = grid[0].length;
+		int res = 0;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] == '1') {
+					res++;
+					dfs(grid, i, j);
+				}
+			}
+		}
+
+		return res;
+	}
+
+	private void dfs(char[][] grid, int i, int j) {
+		if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != '1') return;
+		grid[i][j] = '0';
+		dfs(grid, i - 1, j);
+		dfs(grid, i + 1, j);
+		dfs(grid, i, j - 1);
+		dfs(grid, i, j + 1);
+	}
+}
