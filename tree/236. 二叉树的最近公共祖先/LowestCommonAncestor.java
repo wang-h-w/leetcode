30332751f5858cfd430b3bf5066c10ddd82dea39
Haowen Wang <wang.h.w@outlook.com>
2022-07-27 13:30:11 +0800

    complete 236

---
+++ tree/236. 二叉树的最近公共祖先/LowestCommonAncestor.java
@@ -0,0 +1,26 @@
+class LowestCommonAncestor {
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) {
+			this.val = val;
+		}
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root == null || root.val == p.val || root.val == q.val) return root;
+		TreeNode left = lowestCommonAncestor(root.left, p, q);
+		TreeNode right = lowestCommonAncestor(root.right, p, q);
+		if (left == null && right != null) return right;
+		else if (left != null && right == null) return left;
+		else if (left == null && right == null) return null;
+		return root;
+	}
+}
