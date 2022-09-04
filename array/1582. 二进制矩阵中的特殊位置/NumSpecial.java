c832433dbe109b612a3eeff41bd5d20031f49a55
Haowen Wang <wang.h.w@outlook.com>
2022-09-04 21:04:56 +0800

    complete 1582

---
+++ array/1582. 二进制矩阵中的特殊位置/NumSpecial.java
@@ -0,0 +1,18 @@
+class NumSpecial {
+	public int numSpecial(int[][] mat) {
+		int n = mat.length, m = mat[0].length, ans = 0;
+		int[] r = new int[n], c = new int[m];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				r[i] += mat[i][j];
+				c[j] += mat[i][j];
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (mat[i][j] == 1 && r[i] == 1 && c[j] == 1) ans++;
+			}
+		}
+		return ans;
+	}
+}
