761aaee912389e5a7188dddfe08663a3bba72e6e
Haowen Wang <wang.h.w@outlook.com>
2022-09-16 08:49:43 +0800

    complete 672

---
+++ math/672. 灯泡开关2/FlipLights.java
@@ -0,0 +1,8 @@
+class FlipLights {
+	public int flipLights(int n, int presses) {
+		if (presses == 0) return 1;
+        if (n == 1) return 2;
+        else if (n == 2) return presses == 1 ? 3 : 4;
+        else return presses == 1 ? 4 : presses == 2 ? 7 : 8;
+	}
+}
