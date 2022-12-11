3c49f41a5e42de00afeaad21ddfac3c1379f6fc2
Haowen Wang <wang.h.w@outlook.com>
2022-12-11 09:47:04 +0800

    complete 1827

---
+++ greedy/1827. 最少操作使数组递增/MinOperations.java
@@ -0,0 +1,11 @@
+class MinOperations {
+	public int minOperations(int[] nums) {
+		int res = 0;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i] > nums[i - 1]) continue;
+			res += nums[i - 1] + 1 - nums[i];
+			nums[i] = nums[i - 1] + 1;
+		}
+		return res;
+	}
+}
