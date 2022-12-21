782b4a16ba7dc6a78ffc5c215f5c65876bd8ff7b
Haowen Wang <wang.h.w@outlook.com>
2022-12-21 10:09:15 +0800

    complete 1760

---
+++ search/1760. 袋子里最少数目的球/MinimumSize.java
@@ -0,0 +1,13 @@
+class MinimumSize {
+	public int minimumSize(int[] nums, int maxOperations) {
+		int left = 1, right = (int)1e9;
+		while (left < right) {
+			int mid = (left + right) >>> 1;
+			long s = 0;
+			for (int n : nums) s += (n - 1) / mid;
+			if (s <= maxOperations) right = mid;
+			else left = mid + 1;
+		}
+		return left;
+	}
+}
