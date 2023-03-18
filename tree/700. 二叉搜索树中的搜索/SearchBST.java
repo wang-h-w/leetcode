c7369aaf09f269e7676aab31e1437711d2bbdd40
Haowen Wang <wang.h.w@outlook.com>
2023-03-18 19:04:47 +0800

    complete 700

---
+++ tree/700. 二叉搜索树中的搜索/SearchBST.java
@@ -0,0 +1,8 @@
+class SearchBST {
+	public TreeNode searchBST(TreeNode root, int val) {
+		if (root == null) return root;
+		if (root.val == val) return root;
+		else if (root.val < val) return searchBST(root.right, val);
+		else return searchBST(root.left, val);
+	}
+}
